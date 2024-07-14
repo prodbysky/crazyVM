03aaf8e67591ed0a1b2889a7c603c5c34ae7cf34
prodbysky <kyrbynyt@gmail.com>
2024-07-14 04:28:08 +0300

    Reading input file to assembler and stripping out comments

--- src/bin/crassembler/main.rs
+++ src/bin/crassembler/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello from the assembler for crazyVM!");
+use clap::Parser;
+use std::error::Error;
+
+// Casm assembler for the crazyVM VM
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Casm file name to assemble
+    #[arg(short, long = "input")]
+    input_file: String,
+
+    /// Output filename
+    #[arg(short, long = "output")]
+    output_file: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    let source = std::fs::read_to_string(args.input_file)?;
+    let source = source
+        .lines()
+        .filter(|line| !line.starts_with(';'))
+        .collect::<Vec<_>>();
+
+    Ok(())
 }
