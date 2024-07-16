4f544f4758b1fb6fc89af0762e7adf53784edfe4
prodbysky <kyrbynyt@gmail.com>
2024-07-16 20:31:52 +0300

    Remove debug print

--- vm/src/machine.rs
+++ vm/src/machine.rs
@@ -82,8 +82,6 @@ impl CrazyVM {
             .get_next_instruction()
             .ok_or(RuntimeError::NoNextInstruction)?;
 
-        eprintln!("{}", ins);
-
         match ins {
             Opcode::Add(r1, r2, r3) => {
                 self.registers[r3] = self.registers[r1] + self.registers[r2];
